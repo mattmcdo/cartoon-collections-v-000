8a0e10909daf4a785b99590edfe662fc49c53a8b
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-08-27 01:47:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 end
 
 def long_planeteer_calls(calls)
-  calls.find {|chant.index| chant.length > 4}
+  calls.find_index {|chant| chant.length > 4}
 end
 
 def find_the_cheese# code an argument here
