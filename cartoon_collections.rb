faca8985f4dbfc13d0827f522ea9605de12eafd7
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-08-27 01:29:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index { |dwarf, index| puts "#{index}. #{dwarf}"}
   end
-end
 
 def summon_captain_planet# code an argument here
   # Your code here
